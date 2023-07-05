59f89b41b9870bcb710c858d996bc8c542981e7a
WilliansCMathias <w.mathias@socialdigitalcommerce.com.br>
2023-07-05 15:17:28 -0300

    feat: header

---
+++ react/components/CustomMinicartFooter/types.ts
@@ -0,0 +1,7 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface MinicartProps {
+  cupomDesconto: boolean
+  cupomVendedor: boolean
+  frete: boolean
+}
