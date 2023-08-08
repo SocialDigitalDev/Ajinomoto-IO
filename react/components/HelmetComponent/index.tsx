d3a7820c761561da366873fff7392fbf16635b63
Jonas Di Pierro <j.dipierro@socialdigitalcommerce.com.br>
2023-08-08 17:16:56 -0300

    Favicon instalado

--- react/components/HelmetComponent/index.tsx
+++ react/components/HelmetComponent/index.tsx
@@ -7,19 +7,11 @@ const HelmetComponent = () => {
 	return (
 		// @ts-ignore
 		<Helmet>
-			<meta
-				name="viewport"
-				content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-			></meta>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 			<meta name="theme-color" content="#000" />
-			<meta
-				name="adopt-website-id"
-				content="d634b0e0-fbb8-4bb8-9efa-bb364f882777"
-			/>
-			<script
-				src="//tag.goadopt.io/injector.js?website_code=d634b0e0-fbb8-4bb8-9efa-bb364f882777"
-				className="adopt-injector"
-			></script>
+			<link rel="shortcut icon" href="/arquivos/lojaajinomoto-favicon.ico" />
+			<meta name="adopt-website-id" content="d634b0e0-fbb8-4bb8-9efa-bb364f882777" />
+			<script	src="//tag.goadopt.io/injector.js?website_code=d634b0e0-fbb8-4bb8-9efa-bb364f882777" className="adopt-injector"></script>
 		</Helmet>
 	);
 };
