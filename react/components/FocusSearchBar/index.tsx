39204d9508cde5098a2535979883a4aef81f42f7
WilliansCMathias <w.mathias@socialdigitalcommerce.com.br>
2023-08-14 11:05:48 -0300

    fix: h2 beneficios

---
+++ react/components/FocusSearchBar/index.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+
+const FocusSearchBar = () => {
+	useEffect(() => {
+		// const triggerSearch: HTMLElement | null = document.querySelector(
+		// 	".vtex-modal-layout-0-x-triggerContainer--search-fixed-desktop"
+		// );
+		const inputSearch: any = document.querySelector(
+			".vtex-store-components-3-x-autoCompleteOuterContainer .vtex-styleguide-9-x-input"
+		);
+
+		if (inputSearch) {
+			setTimeout(() => {
+				console.log("aqui>>>>>>>");
+				inputSearch.focus();
+			}, 1000);
+		}
+	}, []);
+
+	return null;
+};
+
+export default FocusSearchBar;
