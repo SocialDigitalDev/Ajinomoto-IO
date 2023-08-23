489ab1c6acff0ff47ec2b01ce21870916bc2e1ec
Jonas Di Pierro <j.dipierro@socialdigitalcommerce.com.br>
2023-08-23 10:15:24 -0300

    Clicar fora do botão de sabores

--- react/components/SimilarProducts/index.tsx
+++ react/components/SimilarProducts/index.tsx
@@ -1,28 +1,31 @@
-import React, { useContext, useEffect, useState } from "react"
+import React, { useContext, useEffect, useState, useRef } from "react"
 import { ProductContext } from "vtex.product-context"
 
 import "./global.css"
 
 const SimilarProducts = () => {
-    
+
     const [isLoaded, setIsLoaded] = useState(false);
     const [sabor, setSabor] = useState<any>();
     const [openClass, setOpenClass] = useState("closed");
-    const productContext = useContext(ProductContext)
-    const prodID = productContext?.product?.productId
-    
+    const productContext = useContext(ProductContext);
+    const prodID = productContext?.product?.productId;
+    const wrapperRef = useRef(null);
+
+    // Faz a requisição e e armazena o resultado numa variável de useState.
     useEffect(() => {
         fetch('/api/catalog_system/pub/products/crossselling/similars/' + prodID)
-        .then((resp) => {
-            return resp.json()
-        })
-        .then((result : any) => {
+            .then((resp) => {
+                return resp.json()
+            })
+            .then((result: any) => {
 
-            let similars = result;
-            listSimilars(similars)
-        })
+                let similars = result;
+                listSimilars(similars)
+            })
     }, [])
 
+    // Esta função seta uma classe como Open e Closed para uso na renderização e no botão.
     function setOpenClose() {
         if (openClass == "closed") {
             setOpenClass("open");
@@ -31,10 +34,31 @@ const SimilarProducts = () => {
         }
     }
 
+    // Esta função seta e limpa um evento de clique para tudo que não for parametrizado por ela.
+    function useOutsideAlerter(ref:any) {
+        useEffect(() => {
+ 
+            function handleClickOutside(event:any) {
+                if (ref.current && !ref.current.contains(event.target)) {
+                    setOpenClass("closed");
+                }
+            }
+
+            document.addEventListener("mousedown", handleClickOutside);
+            return () => {
+                document.removeEventListener("mousedown", handleClickOutside);
+            };
+        }, [ref]);
+    }
+
+    // Executa a função acima e passa o hook useRef como parâmetro e anula seu valor.
+    useOutsideAlerter(wrapperRef);
+
+    // Lista os similares vindo de um array e pré-renderiza numa variável a lista de produtos similares para uso na renderização.
     const listSimilars = (item: any) => {
 
-        let getSabor = item ? item.map(item => 
-            
+        let getSabor = item ? item.map(item =>
+
             <li className="sabor-item">
                 <a href={item.link} >
                     {item.Sabor[0]}
@@ -44,16 +68,17 @@ const SimilarProducts = () => {
         ) : null;
 
         setSabor(getSabor);
-
+        // Verifica se o item existe e se tem conteúdo, e seta o carregamento do componente como verdadeiro.
         if (item.length > 0) {
             setIsLoaded(true)
         } else {
             setIsLoaded(false)
         }
     }
-        
+
+    // Verifica se o carregamento foi definido como verdadeiro e renderiza o componente.
     return isLoaded ? (
-        <div className="content--similar-product">
+        <div className="content--similar-product" ref={wrapperRef}>
             <div className="list-products--similar">
                 <button className={`list-products--button ${openClass}`} onClick={setOpenClose}>Outros sabores</button>
                 <ul className={`list-products--list ${openClass}`}>
@@ -63,5 +88,5 @@ const SimilarProducts = () => {
         </div>
     ) : null
 }
-    
+
 export default SimilarProducts
