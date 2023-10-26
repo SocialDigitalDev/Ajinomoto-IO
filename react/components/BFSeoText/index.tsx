36cf28e2d8feb59ff97deb7fe0120d0ff95b3a53
GabrielTenorio-social <g.tenorio@socialsa.com>
2023-10-26 10:02:29 -0300

    BFSeoText schema

--- react/components/BFSeoText/index.tsx
+++ react/components/BFSeoText/index.tsx
@@ -2,7 +2,11 @@ import React, { useState } from "react"
 
 import './global.css'
 
-const BFSeoText = () => {
+type BFSeoTextProps = {
+  message: string
+}
+
+const BFSeoText = ({ message }: BFSeoTextProps) => {
   const [isTextShown, setIsTextShown] = useState(false);
 
   return (
@@ -21,6 +25,20 @@ Accumsan eget cursus rutrum porttitor felis nulla. Ornare.</p>
 
     </section>
   )
+}
+
+BFSeoText.schema = {
+  title: "Seo Text",
+  description: "descrição",
+  type: "object",
+  properties: {
+    message: {
+      title: "AjinoFriday: descontos incríveis de Black Friday!",
+      description: "Lorem ipsum dolor sit amet consectetur. Tincidunt cursus sociis ac aliquam non lectus. Etiam eu consequat faucibus ac tellus sed in. Integer nisl pellentesque neque ac massa. Vitae amet ullamcorper varius et. Commodo non vitae nunc aliquet amet laoreet gravida feugiat. Lacus facilisi sed at arcu pretium. Quam leo tristique blandit vel pretium. Lorem tempor sit habitant et ac lacus etiam urna in. Tristique nibh tristique lorem et consectetur ut aliquet at ac. Enim mi et dui egestas faucibus orci lacus id. Posuere sit suspendisse in ac arcu pellentesque. Scelerisque malesuada ut eu rhoncus elit mattis. Risus congue mattis aenean morbi aliquam augue ipsum sit. Turpis mauris volutpat elementum arcu in consectetur gravida porttitor ut.Accumsan eget cursus rutrum porttitor felis nulla. Ornare.",
+      type: "string",
+      default: null
+    }
+  }
 }
 
 export default BFSeoText
