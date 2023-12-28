baa18f058164201ceadc974bde2208fe338a7c1e
GabrielTenorio-social <g.tenorio@socialsa.com>
2023-12-28 17:55:04 -0300

    feat: blocos até a primeira prateleira
    - top banner
    - seo section
    - categorias em destaque
    - prateleira

---
+++ react/components/BBBSeoText/index.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { schema } from './schema';
+
+import './global.css';
+
+type BBBSeoTextProps = {
+  textoSEO: string
+}
+
+const BBBSeoText = ({ textoSEO }: BBBSeoTextProps) => {
+  const [isTextShown, setIsTextShown] = useState(false);
+
+  return (
+    <section className="seo-section-text">
+      <div className="text-area">
+        <p className={isTextShown ? 'extended' : 'retracted'}>
+          {textoSEO}
+        </p>
+        { !isTextShown && (
+          <div className="fade" />
+        )}
+      </div>
+      <button
+        className="toggle-text"
+        onClick={() => setIsTextShown(!isTextShown)}
+      >
+        {isTextShown ? 'Ver menos' : 'Ler tudo'}
+      </button>
+    </section>
+  )
+}
+
+BBBSeoText.schema = schema;
+
+export default BBBSeoText;
