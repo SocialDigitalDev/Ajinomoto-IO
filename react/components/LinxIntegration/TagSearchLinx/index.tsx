0077f4bb910de684394649dbe4c43a2da2db0f1d
roger-socialsa <r.vieira@socialsa.com>
2024-07-11 14:16:25 -0300

    fix(project): ajustando colocando tag da linx da page /busca

--- react/components/LinxIntegration/TagSearchLinx/index.tsx
+++ react/components/LinxIntegration/TagSearchLinx/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import waitForEl from "../../../utils/waitForEl";
 
 const TagSearchLinx = () => {
@@ -6,15 +6,13 @@ const TagSearchLinx = () => {
     useEffect(() => {
         const htmlCustom = document.createElement("impulse-search");
 
-        waitForEl(".vtex-store-components-3-x-searchBarInnerContainer", () => {
-            if (!document.querySelector("section.impulse-search-linx > impulse-search"))
-                document.querySelector("section.impulse-search-linx")?.append(htmlCustom);
+        waitForEl("div.render-route-vtex-store-2-x-store-custom-busca", () => {
+            if (!document.querySelector("impulse-search"))
+                document.querySelector("body > div.render-route-vtex-store-2-x-store-custom-busca > div > div.vtex-store__template.bg-base > div > div:nth-child(3) > div > div")?.append(htmlCustom);
         });
     }, [])
 
-    return (
-        <section className="impulse-search-linx"></section>
-    )
+    return null
 
 }
 
